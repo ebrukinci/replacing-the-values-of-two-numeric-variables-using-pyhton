037422ab896684b74288626b9c4c1e54a7aa4ccf
Ebru Kıncı <45835927+ebrukinci@users.noreply.github.com>
2019-06-30 21:47:28 +0300

    Create answer1.py

---
+++ answer1.py
@@ -0,0 +1,8 @@
+a = int(input("Please enter the value of the 1st variable:"))
+b = int(input("Please enter the value of the 2nd variable:"))
+
+a,b = b,a
+
+print("Your First Value:",a,sep ="")
+
+print("Your Second Value:",b,sep ="")
